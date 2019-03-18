998f30e48516437e79e9b260228eb7a03438d949
AzureAD\AlbertVillarOrtiz <albertit8_@hotmail.com>
2019-03-18 23:34:52 +0100

    Skeleton code generated with the folders

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BTCommonModule } from './common/common.module';
+import { BTCoreModule } from './core/core.module';
+import { BTHomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BTCommonModule,
+    BTCoreModule,
+    BTHomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
