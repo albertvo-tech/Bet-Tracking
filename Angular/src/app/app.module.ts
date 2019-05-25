39fb388d3baa8361bed19b986490bbc902b3a90c
AzureAD\AlbertVillarOrtiz <albertit8_@hotmail.com>
2019-05-25 20:24:17 +0200

    merge

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -26,7 +26,7 @@ import { HttpClientModule } from '@angular/common/http';
     BTMatchModule,
     BTVerificationModule,
     NotifyModule,
-    BTCommunityModule
+    BTCommunityModule,
     HttpClientModule
   ],
   providers: [],
